8407bc59ffec5e5dbfd0cad4691a2dee732d6cfb
dongyoung <ehddmleheheh@naver.com>
2019-03-24 22:09:14 +0900

    4주차 실습내용 추가

---
+++ Week4/C_programming_04_07/lab04_7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int max,num1, num2, num3;
+	printf("3개의 정수를 입력하시오 : ");
+	scanf_s("%d%d%d", &num1, &num2, &num3);
+	
+	if (num1 > num2)
+	{
+		max = num1;
+		if (max < num3)
+			max = num3;
+	}
+	else
+	{
+		max = num2;
+		if (max < num3)
+			max = num3;
+	}
+	printf("최대값 = %d\n", max);
+}
